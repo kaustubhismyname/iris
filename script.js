251c5c15b8e8cb602be9812c60fa1f82cd2b0084
Kaustubh <kaustubhismyname@gmail.com>
2024-10-08 11:38:11 +0530

    refactored:   script.js

--- script.js
+++ script.js
@@ -4,12 +4,167 @@ const form = document.querySelector("form");
 const gallery = document.querySelector(".gallery");
 const yearSelector = document.querySelector("#year");
 const monthSelector = document.querySelector("#month");
-const notMonthDay = `<div class="day not-month"></div>`;
-const photoDay = `<div class="day"></div>`;
 const today = new Date(Date.now());
 // prettier-ignore
 const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+function getImageData() {
+  return fetch("images.json")
+    .then((res) => res.json())
+    .then((data) => data);
+}
+
+class App {
+  constructor(data) {
+    this.data = data;
+    this.years = Object.keys(data);
+    this.latestYear = this.years.reduce(
+      (max, item) => (Number(item) >= Number(max) ? Number(item) : max),
+      "0"
+    );
+    this.latestMonth = Object.keys(data[this.latestYear]).reduce(
+      (item, maxMonth) =>
+        months.indexOf(item) >= months.indexOf(maxMonth) ? item : maxMonth,
+      "January"
+    );
+
+    this.setYearSelector(this.years);
+    yearSelector.value = this.latestYear;
+    this.setMonthSelector(data[this.latestYear]);
+
+    // event listeners
+    yearSelector.addEventListener(
+      "change",
+      function () {
+        this.setMonthSelector(data[yearSelector.value]);
+      }.bind(this)
+    );
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.showGallery(monthSelector.value, yearSelector.value, this.data);
+    });
+
+    this.showGallery(monthSelector.value, yearSelector.value, this.data);
+  }
+
+  daysInMonth(month, year) {
+    if (month == "February") {
+      if (year % 4 === 0) {
+        if (year % 100 === 0) return 28;
+        return 29;
+      }
+
+      return 28;
+    } else if (
+      [
+        "January",
+        "March",
+        "May",
+        "July",
+        "August",
+        "October",
+        "December",
+      ].includes(month)
+    ) {
+      return 31;
+    } else return 30;
+  }
+
+  showGallery(month, year, data) {
+    gallery.innerHTML = "";
+
+    const d = Date.parse(`${month} 1 ${year}`);
+    const a = new Date(d);
+    const day = a.getDay();
+    const numDays = this.daysInMonth(month, +year);
+
+    console.log(day);
+
+    for (let i = 0; i < 35; i++) {
+      const src =
+        data[year]?.[month]?.[`${i - day + 1}`]?.url || "/images/no-pic-2.jpg";
+      console.log(src);
+      if (i < day || i > day + numDays - 1) {
+        gallery.insertAdjacentHTML(
+          "beforeend",
+          `<div class="day not-month "data-index='${i}'></div>`
+        );
+      } else {
+        gallery.insertAdjacentHTML(
+          "beforeend",
+          `<div class="day" data-index='${i}' >
+            <div class='date-overlay'>
+               <span class='date-txt'>${i - day + 1}</span>
+               ${
+                 src == "/images/no-pic-2.jpg"
+                   ? `<span class='no-img-txt'>No Image</span>`
+                   : ""
+               }
+            </div>
+          <img src='${src}'class='img-of-the-day'> </img>
+          </div>`
+        );
+      }
+    }
+  }
+
+  setYearSelector(years) {
+    years.forEach((y) => {
+      yearSelector.insertAdjacentHTML(
+        "afterbegin",
+        `<option value="${y}">${y}</option> -->`
+      );
+    });
+  }
+
+  setMonthSelector(year) {
+    monthSelector.innerHTML = "";
+
+    const months = Object.keys(year);
+    months.forEach((m) => {
+      monthSelector.insertAdjacentHTML(
+        "beforeend",
+        `<option value="${m}">${m}</option>`
+      );
+    });
+  }
+}
+
+getImageData().then((data) => {
+  const app = new App(data);
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// OLD CODE --- DONOT UN-COMMENT
+//
 // fetch("years.json")
 //   .then((res) => res.json())
 //   .then((data) => {
@@ -131,126 +286,3 @@ const months =  ["January", "February", "March", "April", "May", "June", "July",
 //     setYearSelector(years);
 //     setMonthSelector(data[years[0]]);
 //   });
-
-function getImageData() {
-  return fetch("images.json")
-    .then((res) => res.json())
-    .then((data) => data);
-}
-
-function setYearSelector(years) {
-  years.forEach((y) => {
-    yearSelector.insertAdjacentHTML(
-      "afterbegin",
-      `<option value="${y}">${y}</option> -->`
-    );
-  });
-}
-
-function setMonthSelector(year) {
-  monthSelector.innerHTML = "";
-
-  const months = Object.keys(year);
-  months.forEach((m) => {
-    monthSelector.insertAdjacentHTML(
-      "beforeend",
-      `<option value="${m}">${m}</option>`
-    );
-  });
-}
-
-class App {
-  constructor(data) {
-    this.data = data;
-    this.years = Object.keys(data);
-    this.latestYear = this.years.reduce(
-      (max, item) => (Number(item) >= Number(max) ? Number(item) : max),
-      "0"
-    );
-    this.latestMonth = Object.keys(data[this.latestYear]).reduce(
-      (item, maxMonth) =>
-        months.indexOf(item) >= months.indexOf(maxMonth) ? item : maxMonth,
-      "January"
-    );
-
-    setYearSelector(this.years);
-    yearSelector.value = this.latestYear;
-    setMonthSelector(data[this.latestYear]);
-
-    // event listeners
-    yearSelector.addEventListener("change", function () {
-      setMonthSelector(data[yearSelector.value]);
-    });
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      this.showGallery(monthSelector.value, yearSelector.value, this.data);
-    });
-
-    this.showGallery(monthSelector.value, yearSelector.value, this.data);
-  }
-
-  daysInMonth(month, year) {
-    if (month == "February") {
-      if (year % 4 === 0) {
-        if (year % 100 === 0) return 28;
-        return 29;
-      }
-
-      return 28;
-    } else if (
-      [
-        "January",
-        "March",
-        "May",
-        "July",
-        "August",
-        "October",
-        "December",
-      ].includes(month)
-    ) {
-      return 31;
-    } else return 30;
-  }
-
-  showGallery(month, year, data) {
-    gallery.innerHTML = "";
-
-    const d = Date.parse(`${month} 1 ${year}`);
-    const a = new Date(d);
-    const day = a.getDay();
-    const numDays = this.daysInMonth(month, +year);
-
-    console.log(day);
-
-    for (let i = 0; i < 35; i++) {
-      const src =
-        data[year]?.[month]?.[`${i - day + 1}`]?.url || "/images/no-pic-2.jpg";
-      console.log(src);
-      if (i < day || i > day + numDays - 1) {
-        gallery.insertAdjacentHTML(
-          "beforeend",
-          `<div class="day not-month "data-index='${i}'></div>`
-        );
-      } else {
-        gallery.insertAdjacentHTML(
-          "beforeend",
-          `<div class="day" data-index='${i}' >
-            <div class='date-overlay'>
-               <span class='date-txt'>${i - day + 1}</span>
-               ${
-                 src == "/images/no-pic-2.jpg"
-                   ? `<span class='no-img-txt'>No Image</span>`
-                   : ""
-               }
-            </div>
-          <img src='${src}'class='img-of-the-day'> </img>
-          </div>`
-        );
-      }
-    }
-  }
-}
-
-getImageData().then((data) => {
-  const app = new App(data);
-});
